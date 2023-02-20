1d67e4e6e16f1ddd8b8ea332141f939acdc6dc68
Bao1612 <87375023+Bao1612@users.noreply.github.com>
2023-02-20 21:45:58 +0700

    bai tap weak 3

---
+++ weak3/hello-react/src/App.js
@@ -0,0 +1,11 @@
+import todoTasks from "./components/todoTask";
+
+function App() {
+  return (
+    <div>
+        <todoTasks/>
+    </div>
+  );
+}
+
+export default App;
---
+++ weak3/hello-react/src/components/todoTask.js
@@ -0,0 +1,9 @@
+function todoTasks() {
+    return (
+        <div>
+            <h2>Enter your task here</h2>
+        </div>
+    )
+}
+
+export default todoTasks;
